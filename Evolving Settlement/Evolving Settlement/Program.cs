8cd28035f24f9c447ac58333abb68da9aa4f8299
LazardStrife <lazardstrife@gmail.com>
2020-03-26 21:56:32 -0500

    added c# solution

---
+++ Evolving Settlement/Evolving Settlement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interface;
+using SimulationModel;
+
+namespace Evolving_Settlement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
